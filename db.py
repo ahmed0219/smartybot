84a3917d57c2e675f409ee58db78174f27498250
Rayen Ben Aziza <rayenbenaziza1234@gmail.com>
2025-04-11 21:38:45 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import chromadb
-DB_NAME = "white_papers1"
+DB_NAME = "white_papers_db"
 
 def get_db():
-    chroma_client = chromadb.PersistentClient(path="./newchroma_db")
+    chroma_client = chromadb.PersistentClient(path="./chroma_db")
     db = chroma_client.get_collection(name=DB_NAME)
     return db
